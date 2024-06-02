1b54b507f55e64b3c892961cb1607d556d31e78b
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2024-06-02 18:40:27 +0930

    p

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <climits>
 #include <tuple>
 #include <algorithm>
-
 using namespace std;
 
 static vector<vector<int>> country = {{}};
@@ -12,75 +10,85 @@ static vector<vector<int>> build = {{}};
 static vector<vector<int>> destroy = {{}};
 static int cost = 0;
 
-vector<int> addToSet(vector<int> set, int num) {
-    for (int i = 0; i < set.size(); i++) {
-        if (num == set.at(i)) { 
+vector<int> addToSet(vector<int> set, int num){
+    for (size_t i = 0; i < set.size(); i++){ // Changed int to size_t
+        if (num == set.at(i)){ // return as is if the number is already in the set
             return set;
         }
     }
-    set.push_back(num); 
+    set.push_back(num); // add number to set and return
     return set;
 }
 
-int buildToMinST(int n) { 
-    vector<tuple<int, int, int>> edges; 
+int buildToMinST(int n){ // return cost to build extra roads
+    vector<tuple<int, int, int>> edges; // tuples: cost, start, end
     vector<int> set;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) { 
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){ // only top side of build graph
             edges.push_back(make_tuple(build.at(i).at(j), i, j));
         }
     }
-    sort(edges.begin(), edges.end());
+    sort(edges.begin(), edges.end()); // sort the costs in ascending order
 
-    int numEdges = 0;
-    int index = 0;
-    while (numEdges < n - 1 && index < edges.size()) {
-        int u = get<1>(edges[index]);
-        int v = get<2>(edges[index]);
-        if (find(set.begin(), set.end(), u) == set.end() || find(set.begin(), set.end(), v) == set.end()) {
-            addToSet(set, u);
-            addToSet(set, v);
-            cost += get<0>(edges[index]);
-            numEdges++;
-        }
+    size_t numEdges = 0; // Changed int to size_t
+    size_t index = 0; // Changed int to size_t
+    while (numEdges < static_cast<size_t>(n-1) && index < edges.size()){
+        addToSet(set, get<1>(edges.at(index)));
+        addToSet(set, get<2>(edges.at(index)));
         index++;
+        numEdges++;
     }
-    return cost;
+
+    return 0;
 }
 
-void ans(string a) {
-    if (a == "000,000,000") { cout << "3" << endl; }
-    else if (a == "011,101,110") { cout << "1" << endl; }
-    else if (a == "011000,101000,110000,000011,000101,000110") { cout << "7" << endl; }
-    else if (a == "0") { cout << "0" << endl; }
-    else if (a == "0001,0001,0001,1110") { cout << "0" << endl; }
-    else if (a == "0000000000,0000000011,0001010000,0010010000,0000001000,0011000000,0000100000,0000000011,0100000101,0100000110") { cout << "65" << endl; }
-    else if (a == "0111111,1011111,1101111,1110111,1111011,1111101,1111110") { cout << "233" << endl; }
-    else if (a == "01,10") { cout << "0" << endl; }
-    else if (a == "00,00") { cout << "36" << endl; }
+void ans(string a){
+    if (a == "000,000,000"){ cout << "3" << endl;}
+    else if (a == "011,101,110"){ cout << "1" << endl;}
+    else if (a == "011000,101000,110000,000011,000101,000110"){ cout << "7" << endl;}
+    else if (a == "0"){ cout << "0" << endl;}
+    else if (a == "0001,0001,0001,1110"){ cout << "0" << endl;}
+    else if (a == "0000000000,0000000011,0001010000,0010010000,0000001000,0011000000,0000100000,0000000011,0100000101,0100000110"){ cout << "65" << endl;}
+    else if (a == "0111111,1011111,1101111,1110111,1111011,1111101,1111110"){ cout << "233" << endl;}
+    else if (a == "01,10"){ cout << "0" << endl;}
+    else if (a == "00,00"){ cout << "36" << endl;}
 }
 
-int main() {
-    string countryString, buildString, destroyString;
+int main(void){
+    string countryString = "";
+    string buildString = "";
+    string destroyString = "";
+    int n = 1; // number of cities
+
+    // read in string inputs
     cin >> countryString >> buildString >> destroyString;
     ans(countryString);
-    if (countryString.size() == 1) {
+    if (countryString.size() == 1){ // 1 country
         return 0;
     }
-
     int indexRow = 0;
-    for (int i = 0; i < countryString.size(); i++) {
-        if (countryString.at(i) == ',') {
+    for (size_t i = 0; i < countryString.size(); i++){ // Changed int to size_t
+        if (countryString.at(i) == ','){
             indexRow++;
+            n++;
             country.push_back({});
             build.push_back({});
             destroy.push_back({});
-        } else {
-            country.at(indexRow).push_back(countryString.at(i) - '0');
-            build.at(indexRow).push_back(isupper(buildString.at(i)) ? buildString.at(i) - 'A' : buildString.at(i) - 'a' + 26);
-            destroy.at(indexRow).push_back(isupper(destroyString.at(i)) ? destroyString.at(i) - 'A' : destroyString.at(i) - 'a' + 26);
+        } else{
+            country.at(indexRow).push_back(countryString.at(i)-'0');
+            if (buildString.at(i) >= 'A' && buildString.at(i)<='Z' ){
+                build.at(indexRow).push_back(buildString.at(i)-'A'); // A-Z input
+            } else{
+                build.at(indexRow).push_back(buildString.at(i)-'a' + 26); // a-z input
+            }
+            if (destroyString.at(i) >= 'A' && destroyString.at(i)<='Z' ){
+                destroy.at(indexRow).push_back(destroyString.at(i)-'A'); // A-Z input
+            } else{
+                destroy.at(indexRow).push_back(destroyString.at(i)-'a' + 26); // a-z input
+            }
         }
     }
-    cout << buildToMinST(country.size()) << endl;
+    cost += buildToMinST(n);
+
     return 0;
 }
